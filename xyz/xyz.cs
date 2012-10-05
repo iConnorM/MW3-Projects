489212f95fd35efce93f72b30d03876c04f25bee
Connor McDonald <c96.mcdonald@gmail.com>
2012-10-05 19:59:51 +0100

    Inital Commit
    Initial Files

---
+++ xyz/xyz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InfinityScript;
+
+namespace xyz
+{
+    public class xyz : BaseScript
+    {
+        public xyz()
+            : base()
+        {
+            PlayerConnected += new Action<Entity>(player =>
+            {
+                HudElem PlayerPos = HudElem.CreateFontString(player, "hudbig", 0.8f);
+                PlayerPos.SetPoint("top left", "top left", 10, 140);
+                PlayerPos.HideWhenInMenu = true;
+
+                OnInterval(500, () =>
+                {
+                    float X = (player.Origin.X);
+                    float Y = (player.Origin.Y);
+                    float Z = (player.Origin.Z);
+                    int PlayerX = (int)X;
+                    int PlayerY = (int)Y;
+                    int PlayerZ = (int)Z;
+
+                    PlayerPos.SetText("Position: X: " + PlayerX + " Y: " + PlayerY + " Z: " + PlayerZ);
+
+                    return true;
+                });
+            });
+        }
+    }
+}
